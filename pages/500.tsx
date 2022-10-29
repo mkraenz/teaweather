bc37aca4165f9503a215ca40dccd22ae4bd945ee
Mirco Kraenz <kraenzmirco@gmail.com>
2022-10-29 16:29:26 +0900

    fix(#1): styled 500 error page

---
+++ pages/500.tsx
@@ -0,0 +1,35 @@
+import { Button, VStack } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { FC } from "react";
+import Heading1 from "../src/components/common/Heading1";
+import Heading2 from "../src/components/common/Heading2";
+import { colorWorkaroundGetServerSideProps } from "../src/components/common/layout/dark-mode-workaround";
+
+interface Props {}
+
+const Custom500: FC<Props> = (props) => {
+  return (
+    <VStack justifyContent={"center"} mt={16} gap={16}>
+      <Heading1 text={"Oops. This shouldn't have happened."} />
+      <Heading2 text={"500 Internal Server Error"} />
+      <Button
+        as={NextLink}
+        href={"/"}
+        display={{ base: "none", md: "inline-flex" }}
+        fontSize={"sm"}
+        fontWeight={600}
+        color={"white"}
+        bg={"pink.400"}
+        _hover={{
+          bg: "pink.300",
+        }}
+      >
+        Return to Home
+      </Button>
+    </VStack>
+  );
+};
+
+export const getServerSideProps = colorWorkaroundGetServerSideProps;
+
+export default Custom500;
