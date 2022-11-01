82736ee4b6c0f392c13845747309771bc9d0e028
Mirco Kraenz <kraenzmirco@gmail.com>
2022-11-01 15:15:42 +0900

    feat(#1): tracer bullet how to auth on api route

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,16 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
+  id: string;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const session = getSession(req, res);
+  if (!session)
+    throw new Error(
+      "No session. Did you forget to wrap the handler function in withApiAuthRequired?"
+    );
+  const { user } = session;
   // TODO include https://openweathermap.org/api
   // TODO get ip https://ipapi.co/free/
-  // TODO add login https://auth0.com/docs/quickstart/webapp/nextjs/01-login
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: "John Doe", id: user.sub });
 }
+export default withApiAuthRequired(handler);
