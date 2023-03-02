0c6ebd8a42fa4a8ed7196fe3bd4a023b666f9209
Mirco Kraenz <software@kraenz.eu>
2023-03-02 18:37:16 +0100

    feat(#4): nicer looking address selection list

--- src/components/common/AddressSelectionModal.tsx
+++ src/components/common/AddressSelectionModal.tsx
@@ -1,6 +1,10 @@
+import { StarIcon, TriangleUpIcon } from "@chakra-ui/icons";
 import {
   Button,
+  Divider,
+  HStack,
   List,
+  ListIcon,
   ListItem,
   Modal,
   ModalBody,
@@ -9,6 +13,9 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Stack,
+  Text,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { useState } from "react";
 
@@ -28,60 +35,97 @@ type Address = {
   };
 };
 
+const AddressListItem = ({
+  onSelect,
+  isSelected,
+  address,
+}: {
+  onSelect: (address: Address) => void;
+  isSelected: boolean;
+  address: Address;
+}) => {
+  const accentColor = useColorModeValue("cyan.500", "cyan.400");
+
+  const bg =
+    // TODO color + dark/light mode
+    isSelected ? accentColor : undefined;
+  return (
+    <>
+      <ListItem onClick={() => onSelect(address)} display={"flex"}>
+        <HStack>
+          <ListIcon
+            as={isSelected ? StarIcon : TriangleUpIcon}
+            color={bg}
+            boxSize={4}
+            transform={isSelected ? undefined : "rotate(90deg)"}
+          />
+          <Stack gap={0}>
+            {/* chakra puts weird margin on the stack elements https://github.com/chakra-ui/chakra-ui/issues/2578#issuecomment-981121992 */}
+            <Text style={{ margin: 0 }}>
+              {address.attributes.PlaceName} - {address.attributes.Type}
+            </Text>
+            <Text style={{ margin: 0 }}>{address.attributes.Place_addr}</Text>
+            <Text style={{ margin: 0 }}>{address.attributes.CntryName}</Text>
+          </Stack>
+        </HStack>
+      </ListItem>
+      <Divider />
+    </>
+  );
+};
+
 const toKey = (address: Address) =>
   `${address.location.x},${address.location.y}`;
-
-const formatAddress = (address: Address) => {
-  const { PlaceName, Type, Place_addr, CntryName } = address.attributes;
-  return `${Type} - ${PlaceName}, ${Place_addr}, ${CntryName}`;
-};
 
 const AddressSelectionModal = ({
   isOpen,
   onClose,
   addresses,
-  selectedAddress,
+  initialSelectedAddress,
   onConfirm,
 }: {
   isOpen: boolean;
   onClose: () => void;
   addresses: Address[];
-  selectedAddress: Address;
+  initialSelectedAddress: Address;
   onConfirm: (address: Address) => Promise<void>;
 }) => {
-  const [selected, select] = useState(selectedAddress);
+  const [selected, select] = useState(initialSelectedAddress);
   // TODO only show 10 results and allow to 'show more results'
   const handleSelect = () => {
     onConfirm(selected);
     onClose();
   };
+  const handleClose = () => {
+    select(initialSelectedAddress);
+    onClose();
+  };
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Select Address</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            {/* TODO make this look nice */}
             <List spacing={3}>
               {addresses.map((address) => {
-                console.log(address);
                 const key = toKey(address);
-                const bg =
-                  // TODO color + dark/light mode
-                  toKey(selected) === toKey(address) ? "green.500" : undefined;
+                const isSelected = toKey(selected) === toKey(address);
                 return (
-                  <ListItem key={key} onClick={() => select(address)} bg={bg}>
-                    {formatAddress(address)}
-                  </ListItem>
+                  <AddressListItem
+                    key={key}
+                    isSelected={isSelected}
+                    onSelect={select}
+                    address={address}
+                  />
                 );
               })}
             </List>
           </ModalBody>
 
           <ModalFooter>
-            <Button mr={3} variant="ghost" onClick={onClose}>
+            <Button mr={3} variant="ghost" onClick={handleClose}>
               Cancel
             </Button>
             <Button variant="solid" onClick={handleSelect}>
